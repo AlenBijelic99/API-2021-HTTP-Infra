622614120f5cf8f58a9ea8c37c498e2641105d5a
Stefano <stefano.pontarolo@heig-vd.ch>
2022-01-13 10:18:58 +0100

    Fixed some error

--- docker-images/nginx-js-image/content/assets/js/quotes.js
+++ docker-images/nginx-js-image/content/assets/js/quotes.js
@@ -3,17 +3,12 @@
 
         function loadQuotes() {
                 $.getJSON( "/api/quotes/chucknorris/", function (quotes) {
-                        console.log(quotes.text);
-                        var message = String(quotes.text);
-                        $("#cn quote").text(message);
-                });
-				
-				$.getJSON( "/api/quotes/dad/", function (quotes) {
-                        console.log(quotes.text);
-                        var message = String(quotes.text);
-                        $("#dad quote").text(message);
+                    console.log(quotes.text);
+                    var message = String(quotes.text);
+                     $("#quote").text(message);
                 });
         };
+
         loadQuotes();
         setInterval (loadQuotes, 5000);
 })(jQuery);
