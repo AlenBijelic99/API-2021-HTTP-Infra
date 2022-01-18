2de8359ae717c88752b6680c00762cbac8e5049b
Stefano <stefano.pontarolo@heig-vd.ch>
2022-01-18 20:04:54 +0100

    Auto stash before revert of "Merge remote-tracking branch 'origin/fb-nginx-reverse-proxy' into fb-nginx-reverse-proxy"

---
+++ 4-ajax-jquery/docker-images/express-image/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const Joke = require('give-me-a-joke');
+
+app.get('/', (req, res) => {
+  res.send('Go to /dad, /chucknorris, /custom?firstname=John&lastname=Smith or /category/[blonde, knock-knock, animal, jod]')
+})
+
+app.get('/dad', (req, res) =>{
+  Joke.getRandomDadJoke (function(joke) {
+    res.json({ text: joke})
+  });
+})
+
+app.get('/chucknorris', (req, res) =>{
+  Joke.getRandomCNJoke (function(joke) {
+    res.json({ text: joke})
+  });
+})
+
+app.get('/custom', (req, res) =>{
+  var firstname = req.query.firstname
+  var lastname = req.query.lastname
+  Joke.getCustomJoke (firstname, lastname, function(joke) {
+    res.json({ text: joke})
+  });
+})
+
+app.get('/category/:category', (req, res) =>{
+  var category = req.params.category
+  Joke.getRandomJokeOfTheDay (category, function(joke) {
+    res.json({ text: joke})
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
